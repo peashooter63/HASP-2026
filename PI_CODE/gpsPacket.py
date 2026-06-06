880ab8914a0558b8e36a958491d4e3400be23bfd
peashooter63 <karennachiu@gmail.com>
2026-06-06 06:50:00 -0700

    Add files via upload

--- PI_CODE/gpsPacket.py
+++ PI_CODE/gpsPacket.py
@@ -1,82 +1,76 @@
-nmeaSentence = '$GPGGA,202212.00,3024.7205,N,09110.7264,W,1,06,1.69,00061,M,-025,M,,*51,'
-sentenceSize = len(nmeaSentence)
+class NMEA_PACKET:
 
-# Get all strings between $ and *
-fixData = nmeaSentence[0]
-newSentence = (nmeaSentence.strip(fixData)).split("*")[-3:-1]
-nmeaFields = (nmeaSentence.strip(fixData)).split("*")[-3:-1]
-nmeaParsing = ("".join(nmeaFields)).split(",")
+    def Parse_NMEA(NMEA_Sentence):
+            
+            NMEA_FIELDS = NMEA_Sentence.split(",")
+            NMEA_FIX = NMEA_FIELDS.index("$GPGGA")
+            
+            #NMEA_Sentence.strip("$GPGGA")
+        # Check the type of NMEA sentence 
+            if NMEA_Sentence.startswith("$GPGGA") or NMEA_Sentence.startswith("GNGGA"):   #Got to fix this, GNGGA may have difference in structure
+                
+                UTC = NMEA_FIELDS[1]
+                if "S" in NMEA_FIELDS:
+                    SOUTH_INDEX = NMEA_FIELDS.index("S")
+                    Latitude = "-" + f"{NMEA_FIELDS[SOUTH_INDEX-1]}"
+                    LATITUDE_DIR = NMEA_FIELDS[SOUTH_INDEX]
+                elif "N" in NMEA_FIELDS:
+                    NORTH_INDEX = NMEA_FIELDS.index("N")
+                    Latitude = f"{NMEA_FIELDS[NORTH_INDEX-1]}"
+                    LATITUDE_DIR = NMEA_FIELDS[NORTH_INDEX]
 
-def parseNmea(nmeaSentence):
-    # Check the type of NMEA sentence and make a new string to write to buffer for UART port
-    if nmeaParsing[0] == "GPGGA" or nmeaParsing[0] == "GNGGA":   #Got to fix this, GNGGA may have difference in structure
-        getUTC = nmeaParsing[1]
-        getLatitude = nmeaParsing[2]
-        getHemisphere1 = nmeaParsing[3]
-        getLongitude = nmeaParsing[4]
-        getHemisphere2 = nmeaParsing[5]
-        mslAltitude = nmeaParsing[9]
-        
-        nmeaSentenceFinal = getUTC + "," + getLatitude + "," + getHemisphere1 + "," + getLongitude + "," + getHemisphere2 + "," + mslAltitude
-        print(f" Here is the new sentence: {nmeaSentenceFinal}")
+                if "W" in NMEA_FIELDS:
+                    WEST_INDEX = NMEA_FIELDS.index("W")
+                    Longitude = "-" + f"{NMEA_FIELDS[WEST_INDEX-1]}"
+                    LONGITUDE_DIR = NMEA_FIELDS[WEST_INDEX]
+                else:
+                    EAST_INDEX = NMEA_FIELDS.index("E")
+                    Longitude = f"{NMEA_FIELDS[EAST_INDEX-1]}"
+                    LONGITUDE_DIR = NMEA_FIELDS[EAST_INDEX]
+
+                Altitude = NMEA_FIELDS[8]
+            
+
+            NMEA_STRING = f"{UTC}" + "," + f"{LATITUDE_DIR}" + "," + f"{Latitude}" + "," + f"{LONGITUDE_DIR}" + "," + f"{Longitude}" + "," + f"{Altitude}" 
+            print("NMEA_STRING")
+            print(NMEA_STRING)
+            return NMEA_STRING
 
 
-# Join strings to make list of chars between $ and * for checksum calculation
-newString = "".join(newSentence)
-nmeaChars = list(newString)
-print(nmeaChars)
+    def Calculate_Checksum(NMEA_SENTENCE):
+        x = 0
+        NMEA_FIX = NMEA_SENTENCE.find('$')
+        checksum_index = NMEA_SENTENCE.find('*')
+        received_checksum = NMEA_SENTENCE[checksum_index+1:checksum_index+3]
+        print("RECEIVED CHECKSUM")
+        print(received_checksum)
+        # XOR between $ and *
+        print("Test my checksum prints for the XOR") 
+        print(NMEA_SENTENCE[NMEA_FIX+1:checksum_index])
+        for i, char in enumerate(NMEA_SENTENCE[NMEA_FIX+1:checksum_index]):
+            #print(char, hex(ord(char))) 
+            x ^= ord(char)
+            
+        Calculated_Checksum = f"{x:02X}"                             
 
-def calculateChecksum(nmeaChars):
+        Nibble_1 = (x & 0XF0 ) >> 4
+        Nibble_2 = (x & 0X0F)
 
-    x = ord(nmeaChars[0])
-    charSize = len(nmeaChars)
-    # XOR between $ and *
-    for i, char in enumerate(nmeaChars[1:charSize],start=1):
-        y = ord(nmeaChars[i])
-        x ^= y
-    
-    #Calculated NMEA checksum
-    packetChecksum = hex(x)
+        Hex_Nibble1 = (hex(Nibble_1))[2:]
+        Hex_Nibble2 = (hex(Nibble_2))[2:]
+        hex_XOR = "".join(Hex_Nibble1+Hex_Nibble2)
+        print("Calculated Checksum XOR")
+        print(hex_XOR)
 
-    nibble1 = (x & 0XF0 ) >> 4
-    nibble2 = (x & 0X0F)
-    hexNibble1 = (hex(nibble1))[2:]
-    hexNibble2 = (hex(nibble2))[2:]
-
-    # Join a hex strings
-    hexCalculated = "".join(hexNibble1+hexNibble2)
-    print("Calculated Hex Checksum",hexCalculated )
-
-    # Computed ASCII Checksum
-    #asciiChecksum = bytes.fromhex(hexNibble1 + hexNibble2).decode("utf-8")  
-    #print(f"Computed Ascii Checksum: {asciiChecksum}")
-
-    # Received NMEA checksum
-
-    nmeaSentence = '$GPGGA,202212.00,3024.7205,N,09110.7264,W,1,06,1.69,00061,M,-025,M,,*51,'
-    nmeaSize = len(nmeaSentence)
-    checksumString = nmeaSentence[nmeaSize-3:nmeaSize-1]  #can change to -4
-    hexPayloadChecksum = hex(int(checksumString,16))[2:]
-
-    print("Received Hex Checksum",hexPayloadChecksum )
-
-    # Received ASCII Checksum
-    #receivedChecksum = bytes.fromhex(hexPayloadChecksum).decode("utf-8")
-    #print(f"Received Ascii Checksum: {receivedChecksum}")
-
-    if hexPayloadChecksum == hexCalculated:
-        print("Checksum matches")
-    else:
-        print("Data is corrupted")
+        if hex_XOR == received_checksum:
+            print("Checksum matches")
+        else:
+            print("NMEA Data is corrupted")
    
-def parseNmea(nmeaSentence):
-    #After byte array written to serial port and decode
-    print("")
+
+
+# Test sentence  (WIll USE A .STRIP ON THIS.)
+NMEA_TEST_SENTENCE = '$GPGGA,202212.00,3024.7205,N,09110.7264,W,1,06,1.69,00061,M,-025,M,,*51,'
 
 
 
-    # Call method to assign data to fields 
-parseNmea(nmeaParsing)
-
-    # Call method to calculate checksums 
-calculateChecksum(nmeaChars)
